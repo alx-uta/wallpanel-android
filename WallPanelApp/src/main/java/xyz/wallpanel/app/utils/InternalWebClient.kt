c78bb904131a77de88dfe407708691ff1fcde6f6
Alex Uta <marian.alexandru.uta@gmail.com>
2025-12-19 20:40:07 +0000

    use property reference operator for lateinit dialogUtils check

--- WallPanelApp/src/main/java/xyz/wallpanel/app/utils/InternalWebClient.kt
+++ WallPanelApp/src/main/java/xyz/wallpanel/app/utils/InternalWebClient.kt
@@ -83,7 +83,7 @@ open class InternalWebClient(val resources: Resources, private val callback: Web
                     resources.getString(R.string.dialog_message_ssl_not_yet_valid)
             }
             message += resources.getString(xyz.wallpanel.app.R.string.dialog_message_ssl_continue)
-            if (dialogUtils.isInitialized) {
+            if (::dialogUtils.isInitialized) {
                 dialogUtils.showAlertDialog(view.context,
                     resources.getString(R.string.dialog_title_ssl_error),
                     resources.getString(R.string.dialog_message_ssl_continue),
